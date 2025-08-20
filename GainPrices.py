543fe091cbe3fed7cbe36a5e4e8ffded2edf32d3
poctak <poctakt@seznam.cz>
2025-08-20 11:53:52 +0200

    pole pairs

--- GainPrices.py
+++ GainPrices.py
@@ -4,6 +4,8 @@ import logging
 from datetime import datetime, timezone
 from elasticsearch import Elasticsearch
 
+pairs = ["BTCUSDC", "ETHUSDC", "PEPEUSDC"]
+
 # Nastavení logování
 logging.basicConfig(
     level=logging.INFO,
@@ -15,44 +17,46 @@ logging.basicConfig(
 es = Elasticsearch("http://localhost:9200")  # uprav pokud máš jinou adresu
 INDEX_NAME = "my-crypto-index"
 async def fetch_last_trade_loop():
-    url = "https://api.binance.com/api/v3/trades?symbol=BTCUSDC&limit=1"
+
 
     while True:
-        try:
-            response = requests.get(url)
-            response.raise_for_status()  # ošetří chyby HTTP
-            data = response.json()
-            if data:
-                last_trade = data[0]
-                price = float(last_trade['price'])
-                qty = float(last_trade['qty'])
-                timestamp = datetime.now(timezone.utc).isoformat()
+        for pair in pairs:
+            url = "https://api.binance.com/api/v3/trades?symbol=" + pair + "&limit=1"
+            try:
+                response = requests.get(url)
+                response.raise_for_status()  # ošetří chyby HTTP
+                data = response.json()
+                if data:
+                    last_trade = data[0]
+                    price = float(last_trade['price'])
+                    qty = float(last_trade['qty'])
+                    timestamp = datetime.now(timezone.utc).isoformat()
 
-                # Vložení do Elasticsearch
-                doc_price = {
-                    "pair": "BTCUSDC",
-                    "timestamp": timestamp,
-                    "title": "price",
-                    "value": price
-                }
-                doc_qty = {
-                    "pair": "BTCUSDC",
-                    "timestamp": timestamp,
-                    "title": "quantity",
-                    "value": qty
-                }
+                    # Vložení do Elasticsearch
+                    doc_price = {
+                        "pair": pair,
+                        "timestamp": timestamp,
+                        "title": "price",
+                        "value": price
+                    }
+                    doc_qty = {
+                        "pair": pair,
+                        "timestamp": timestamp,
+                        "title": "quantity",
+                        "value": qty
+                    }
 
-                es.index(index=INDEX_NAME, document=doc_price)
-                es.index(index=INDEX_NAME, document=doc_qty)
+                    es.index(index=INDEX_NAME, document=doc_price)
+                    es.index(index=INDEX_NAME, document=doc_qty)
 
-                logging.info(f"💰 Poslední cena: {price:,.2f} USDC | 📊 Objem: {qty}")
-            else:
-                logging.warning("⚠️ Žádná data")
-        except Exception as e:
-            logging.error(f"❌Chyba: {e}")
+                    logging.info(f"💰 Poslední cena: {price:,.2f} USDC | 📊 Objem: {qty}")
+                else:
+                    logging.warning("⚠️ Žádná data")
+            except Exception as e:
+                logging.error(f"❌ Chyba: {e}" )
 
-        await asyncio.sleep(5)
+            await asyncio.sleep(2)
 
 if __name__ == "__main__":
-    logging.info("🚀 Spouštím sledování BTC/USDC...")
+    logging.info("🚀 Spouštím sledování...")
     asyncio.run(fetch_last_trade_loop())
